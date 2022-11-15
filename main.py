5796cfb18b6c3562a3e779c2fda0f6169258e45c
roylau98 <roylau98@gmail.com>
2022-11-15 10:24:50 +0800

    done

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tkinter as tk
 def main():
     root = tk.Tk()
     root.geometry("1280x720")
-    root.title("Vault")
+    root.title("LastCrypt")
     root.rowconfigure(1, weight=1)
     root.columnconfigure(1, weight=1)
     loginFrame(root).grid(row=1, column=1)  # side="top", fill="both", expand=True)
